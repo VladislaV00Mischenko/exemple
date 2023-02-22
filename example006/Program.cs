e0205c35dc43586ba6d96af0d65b7ea71aaa6237
vladislav <rexx891@gmail.com>
2023-02-22 21:13:45 +0300

    написали программу рисующую треугольники

--- example006/Program.cs
+++ example006/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 4;
+int b = 5;
+int c = 7;
+int d = 3;
+int e = 11;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
